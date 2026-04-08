e809b115e3a5f26577b63e8b2706c9de10da589c
Finn <finn@example.com>
2026-04-08 10:14:07 +0800

    Auto-open neo-tree on startup

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  cmd = "Neotree",
+  event = "VimEnter",
   keys = {
     { ";n", "<cmd>Neotree toggle<cr>", desc = "文件树" },
     { ";N", "<cmd>Neotree reveal<cr>", desc = "定位当前文件" },
@@ -27,4 +27,13 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
+    -- 启动时自动打开文件树
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        vim.cmd("Neotree show")
+      end,
+    })
+  end,
 }
