7002ea9dd89ca2e0bcf75e5d10906aa2935e4836
Finn <finn@example.com>
2026-04-14 09:28:43 +0800

    整理 init 配置格式

--- init.lua
+++ init.lua
@@ -45,32 +45,32 @@ vim.o.mousemodel = "popup" -- 右键弹出菜单
 -- 引导 lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- 加载插件
 require("lazy").setup("plugins", {
-  install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	install = { colorscheme = { "tokyonight" } },
+	checker = { enabled = true },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
 
 -- 主题 (插件加载后)
@@ -80,3 +80,4 @@ vim.cmd("colorscheme tokyonight")
 require("config.keymaps")
 -- 自动命令
 require("config.autocmds")
+
